128b8d28b3e8d622bbb814ffa70e4b9383543580
Mingjie Wang <mingjiewang@link.cuhk.edu.cn>
2022-04-13 22:12:55 +0800

    upload basic structure

---
+++ src/Front-End/src/views/personalCenter/basicInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text"> Basic Information </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BasicInformation',
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
---
+++ src/Front-End/src/views/personalCenter/changePassword.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text"> Change Password </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ChangePassword',
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
